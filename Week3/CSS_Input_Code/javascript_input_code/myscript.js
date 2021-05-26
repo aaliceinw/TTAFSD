acd758846dda741de4dedd41ed6e4369685efbd4
aaliceinw <84022510+aaliceinw@users.noreply.github.com>
2021-05-26 11:48:02 +0100

    Week 3 Initial
    Week 3
    .ccs basics
    .js basics
    created cmedemo page

---
+++ Week3/CSS_Input_Code/javascript_input_code/myscript.js
@@ -0,0 +1,33 @@
+alert("Welcome to Javascript class");
+
+//use this to write comments in the code
+/* this is how we write multipline comments in the code*/
+
+//Javascript variable
+var data1;
+var data2,data3,data4;
+
+// storename
+var name = "Vaibhavi"; // String => ""
+var number = 100; // Number
+
+
+//method 1
+var n1 = "Cat";
+
+//method 2
+var n2;
+n2 = "Kittty";
+
+//method 3
+n3 = 12;
+
+//---------------------------
+
+// display output
+document.write("This is how to display javascript output. ");
+document.writeln("")
+document.write("My Name "+n1);
+document.writeln("");
+document.writeln("hello"); //newline
+document.writeln("bye");
